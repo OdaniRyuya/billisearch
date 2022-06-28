6689b9ddb2fbd991c2d6fc32f6bbfb3ade8ae9e9
ryuya13579 <gesuyabami@icloud.com>
2022-06-28 19:58:19 +0900

    a

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,7 @@
 require_relative "boot"
 
 require "rails/all"
-require 'devise'
-require 'kaminari'
-require 'carrierwave'
-require 'capybara/rspec'
+
 # require 'ransack'
 
 # Require the gems listed in Gemfile, including any gems
