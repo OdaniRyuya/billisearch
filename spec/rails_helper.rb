6cc2270d799931e68e7b2777e28eb9c515d047aa
ryuya13579 <gesuyabami@icloud.com>
2022-06-29 14:14:18 +0900

    Delete 不要ファイルの削除

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,4 +72,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.after(:all) do
+    if Rails.env.test?
+      FileUtils.rm_rf("#{Rails.root}/public/uploads/user")
+    end
+  end
 end
